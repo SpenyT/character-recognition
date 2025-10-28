8ca17b9b0405a9247e5c95efeb6ff7ca0adaf2dd
Holocronic <106043811+Holocronic@users.noreply.github.com>
2025-10-28 18:25:05 -0400

    fix: made default activation more robust

--- src/mai/activations/default.py
+++ src/mai/activations/default.py
@@ -3,7 +3,7 @@ from typing import Callable, Tuple
 from mai.activations import register_activ
 
 @register_activ(None)
-def _none():
-    def f(x):  return x
-    def df(x): return np.ones_like(x)
+def _none() -> Tuple[Callable, Callable]:
+    def f(x: np.ndarray) -> np.ndarray:  return x
+    def df(x: np.ndarray)-> np.ndarray: return np.ones_like(x)
     return f, df
