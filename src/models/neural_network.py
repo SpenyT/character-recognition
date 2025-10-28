00dc1078184ac42bd9f534cec69e644466e99323
Holocronic <106043811+Holocronic@users.noreply.github.com>
2025-10-28 01:11:24 -0400

    fix!: major NeuralNetwork class changes - added typing - added type check to add() function - removed use() function - fixed train() function - added evaluate() function

--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -1,19 +1,21 @@
-
+import numpy as np
+from layers import Layer
+from .utils import resolve_loss
 
 
 
 class NeuralNetwork:
-    def __init__(self, loss=None, loss_prime=None):
-        self.layers = []
-        self.loss = loss
-        self.loss_prime = loss_prime
+    def __init__(self, model : np.ndarray[Layer] = [], loss: str = "mse"):
+        self.layers = model
+        self.loss, self.loss_prime = resolve_loss(loss)
 
     def add(self, layer):
+        if not isinstance(layer, Layer):
+            raise TypeError(
+                f"Cannot add object of type {type(layer)}. "
+                f"Only objects that inherit from 'Layer' are supported."
+            )
         self.layers.append(layer)
-    
-    def use(self, loss, loss_prime):
-        self.loss = loss
-        self.loss_prime = loss_prime
 
     def forward_prop(self, input_data):
         for layer in self.layers:
@@ -24,24 +26,32 @@ class NeuralNetwork:
         for layer in reversed(self.layers):
             loss_grad = layer.backward_prop(loss_grad, learning_rate)
 
-    def train_options(self, loss, loss_prime):
-        self.loss = loss
-        self.loss_prime = loss_prime
+    def train(self, x_train, y_train, epochs: int, learning_rate: float, print_output: bool = True):
+        x_train = np.asarray(x_train)
+        y_train = np.asarray(y_train)
 
-    def train(self, x_train, y_train, epochs, learning_rate, print_output: True):
+        print("Starting training...")
         for epoch in range(epochs):
-            err = 0
+            err = 0.0
+            correct = 0
             for x, y in zip(x_train, y_train):
-                output = self.forward_prop(x)
-                err += self.loss(y, output)
-                grad = self.loss_prime(y, output, learning_rate)
-            err /= len(x_train)
-            print(f'Epoch {epoch+1}/epochs    Error={err:.6f}')
-    
+                x = x.reshape(1, -1)
+                logits = self.forward_prop(x)
+                err += self.loss(np.array([y]), logits)
+                grad = self.loss_prime(np.array([y]), logits)
+                self.backward_prop(grad, learning_rate)
 
-    def predict(self, x_test):
-        results = []
-        for x in x_test:
-            output = self.forward_prop(x)
-            results.append(output)
-        return results
+                pred = int(np.argmax(logits, axis=1)[0])
+                if pred == int(y): correct += 1
+
+            err /= len(x_train)
+            train_acc = correct / len(x_train)
+            print(f"Epoch {epoch+1}/{epochs} - loss: {err:.4f} - acc: {train_acc:.4f}")
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        logits = self.forward_prop(np.asarray(X))
+        return np.argmax(logits, axis=1)
+
+    def evaluate(self, X: np.ndarray, y: np.ndarray) -> float:
+        preds = self.predict(X)
+        return float((preds == y).mean())
