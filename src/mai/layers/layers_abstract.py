79c218b589486c767e16a548db526df5b4dc35cb
Holocronic <106043811+Holocronic@users.noreply.github.com>
2025-11-01 15:02:27 -0400

    wip: TrainableLayer base class

--- src/mai/layers/layers_abstract.py
+++ src/mai/layers/layers_abstract.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Iterable, Protocol, Callable, Tuple
+from typing import Iterable, Protocol, List, Tuple, runtime_checkable
 import numpy as np
 
 class Layer(ABC):
+    #is_training: bool = True
     @abstractmethod
     def forward_prop(self, x) -> np.ndarray:
         ...
@@ -15,3 +17,19 @@ class Trainable(Protocol):
     def params(self) -> Iterable[np.ndarray]: ...
     @property
     def grads(self) -> Iterable[np.ndarray]: ...
+
+# Will be implementing this soon
+# class TrainableLayer(Layer, Trainable, ABC):
+#     def __init__(self) -> None:
+#         self._params: List[np.ndarray] = []
+#         self._grads: List[np.ndarray] = []
+
+#     @property
+#     def params(self) -> Tuple[np.ndarray, ...]:
+#         return tuple(self._params)
+    
+#     @property
+#     def grads(self) -> Tuple[np.ndarray, ...]:
+#         return tuple(self._grads)
+
+    
